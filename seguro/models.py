249d8cabf427e2eeb302db34d5e3975cb76a70ff
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-05-21 02:40:33 -0400

    arreglo de admin str

--- seguro/models.py
+++ seguro/models.py
@@ -53,3 +53,6 @@ class Segurocosto(models.Model):
   servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
   costo = models.DecimalField(decimal_places=2, max_digits=10, default=0)
   estado = models.BooleanField(default=True)
+
+  def __str__(self):
+      return '{} - {}'.format(self.seguro.nombre, self.servicio.nombre)
