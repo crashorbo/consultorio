8c13230e91a007f94e8af61e3e1fffda88447d1d
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-05-21 02:26:02 -0400

    agregar segurocosto a admin

--- seguro/admin.py
+++ seguro/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Seguro
+from .models import Seguro, Segurocosto
 # Register your models here.
 admin.site.register(Seguro)
+admin.site.register(Segurocosto)
