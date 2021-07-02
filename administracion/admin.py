74e4303dd0e8576d509b562f0ba87a4d8edd620b
Alexander Calani <alexander.calani.apaza@gmail.com>
2021-07-02 16:34:07 -0400

    cambio en la seccion admin ingresoseguro ingresoparticular

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import IngresoParticular, IngresoSeguro
 # Register your models here.
+admin.site.register(IngresoSeguro)
+admin.site.register(IngresoParticular)
